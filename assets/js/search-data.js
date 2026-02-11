049046fe7dadde3d2872a21fb7ffc02a47326ecf
Aishwarya2067 <aishwaryapathak2067@gmail.com>
2026-02-11 04:20:57 +0000

    Deploying to gh-pages from @ Aishwarya2067/aishwarya2067.github.io@2e96079c6b4f6d7fb10f9d69c064ebb9e62edad6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,10 +36,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
